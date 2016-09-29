1ff5387d1e907bceca87741b172cfa7727a80003
Andy <andy19950@gmail.com>
2016-09-29 21:37:30 +0800

    add Leibniz formula

--- computepi.c
+++ computepi.c
@@ -140,11 +140,11 @@ double compute_pi_Leibniz_avx(size_t N)
     ymm4 = _mm256_setzero_pd();
 
     for (int i = 0; i <= N - 4; i += 4) {
-	ymm3 = _mm256_set_pd(i, i+1.0, i+2.0, i+3.0);
-	ymm3 = _mm256_mul_pd(ymm1,ymm3);
-	ymm3 = _mm256_add_pd(ymm3,ymm2);
-	ymm3 = _mm256_div_pd(ymm0,ymm3);
-	ymm4 = _mm256_add_pd(ymm3,ymm4);		 
+        ymm3 = _mm256_set_pd(i, i+1.0, i+2.0, i+3.0);
+        ymm3 = _mm256_mul_pd(ymm1,ymm3);
+        ymm3 = _mm256_add_pd(ymm3,ymm2);
+        ymm3 = _mm256_div_pd(ymm0,ymm3);
+        ymm4 = _mm256_add_pd(ymm3,ymm4);
     }
     double tmp[4] __attribute__((aligned(32)));
     _mm256_store_pd(tmp, ymm4);
